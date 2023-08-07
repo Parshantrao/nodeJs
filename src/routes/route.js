f8908bc6d4fc20059361fa5357bae39304da73ae
Parshantrao <parshantrao2017@gmail.com>
2023-08-07 14:21:29 +0530

    only one route

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,10 +2,8 @@ const express=require("express");
 const pythonScript = require("../controller/pythonController");
 const router=express.Router()
 
-router.get("/",(req,res)=>{
-    res.send("running")
-})
-router.get("/read",pythonScript)
+
+router.get("/",pythonScript)
 
 
 module.exports= router
